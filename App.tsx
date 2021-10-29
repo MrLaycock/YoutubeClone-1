96e0c633a41644092ac173b9c29cd9a4a093643d
Andrew Laycock <developer@gmail.com>
2021-10-29 21:52:48 +0100

    sign out added

--- App.tsx
+++ App.tsx
@@ -3,16 +3,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+// all of this is amplify stuff
 import Amplify from 'aws-amplify'
 import config from './src/aws-exports'
 Amplify.configure(config)
+import {withAuthenticator} from 'aws-amplify-react-native'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import VideoScreen from './screens/VideoScreen';
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -27,3 +29,6 @@ export default function App() {
     );
   }
 }
+
+// this is a different type of export to get the authenticator working
+export default withAuthenticator(App);
